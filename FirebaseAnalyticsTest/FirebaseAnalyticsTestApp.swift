a7283ed0a90c6a95bb8e78b2b8518836ef639350
Igor <chumikov.igor@mail.ru>
2024-09-02 11:05:43 +0300

    Add

---
+++ FirebaseAnalyticsTest/FirebaseAnalyticsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAnalyticsTestApp.swift
+//  FirebaseAnalyticsTest
+//
+//  Created by Игорь Чумиков on 02.09.2024.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAnalyticsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
