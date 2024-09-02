73cf43358f41635b23342dad43dfbdbf2c7a6518
Igor <chumikov.igor@mail.ru>
2024-09-02 12:30:47 +0300

    add Firebase

--- FirebaseAnalyticsTest/FirebaseAnalyticsTestApp.swift
+++ FirebaseAnalyticsTest/FirebaseAnalyticsTestApp.swift
@@ -6,12 +6,28 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct FirebaseAnalyticsTestApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
+   // @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
 }
+
+//class AppDelegate: NSObject, UIApplicationDelegate {
+//  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+//    FirebaseApp.configure()
+//
+//    return true
+//  }
+//}
